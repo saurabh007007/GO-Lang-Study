3afd6e6c5664158af464dfe8b9750c7a9951cd42
saurabh007007 <loverboybhol143@gmail.com>
2025-11-20 10:07:14 +0530

    added the basic and note to add how external packages are added

---
+++ 02-basic/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+// conditional go lang
+
+// func odd(num int) bool {
+// 	return num%2 != 0
+// }
+
+// func main() {
+// 	fmt.Print(odd(20))
+// 	i := 0
+// 	for i <= 10 {
+// 		fmt.Println(i)
+// 		i++
+// 	}
+// }
+
+func main() {
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+
+}
